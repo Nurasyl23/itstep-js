5262f77fc99f620db44279276b5567646e5072e9
Nurasyl <Nurasyll23@mail.ru>
2021-03-11 15:48:21 +0600

    HW

---
+++ 1/main.js
@@ -0,0 +1,16 @@
+let apples = 0;
+
+apples = 5;
+
+apples = -5;
+
+console.log("В ящике " + apples + " яблок");
+
+  
+if (apples > 0) {
+    console.log("Полная");
+  } else if (apples === 0) { 
+    console.log("Пустая");
+  } else {
+    console.log("Кредитная коробка");
+  }
